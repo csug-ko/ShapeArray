3e048b1c33920b28fd66239a44eae308d95ee6c8
csug-ko <katherine.oconnor@csuglobal.edu>
2025-03-10 00:29:38 +0000

    Update and rename ShapeArray.java.txt to ShapeArray.java

--- ShapeArray.java
+++ ShapeArray.java
@@ -2,26 +2,24 @@ package Shapes;
 
 import java.util.Arrays;
 
-public class ShapeArray
-{
+public class ShapeArray{
+    
     public Object [] shapearray = new Object[3];
-    ShapeArray()
-    {
-
+    
+    // constructor 
+    ShapeArray() {
         double height = 1.2;
         double radius = 1.3;
         shapearray[0] = new Sphere(radius);
         shapearray[1] = new Cone(radius,height);
         shapearray[2] = new Cylinder(height,radius);
-
     }
-    public Object [] getArr()
-    {
+   
+    public Object [] getArr() {
         return shapearray;
     }
 
-    public static void main(String [] args)
-    {
+    public static void main(String [] args) {
         Object [] a = new ShapeArray().getArr();
 
         for(int i = 0; i < 3;i++)
