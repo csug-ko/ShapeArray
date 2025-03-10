cfd954add7c53652a82a4f62906d0b63382c2d90
csug-ko <katherine.oconnor@csuglobal.edu>
2025-03-10 00:24:20 +0000

    Update and rename Shape.java.txt to Shape.java

---
+++ Shape.java
@@ -0,0 +1,17 @@
+package Shapes;
+
+abstract class Shapeb{
+   
+    // constructor 
+    Shape() {
+// shape variables
+        double area;
+        double volume;
+    }
+
+// getter 
+        abstract double surface_area();
+
+// getter
+        abstract double volume();
+}
